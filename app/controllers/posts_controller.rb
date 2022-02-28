6cc48afcb17e6b0f15fb8c1b33343bca5398b720
João Victor Geonizeli <joao.geonizeli@gmail.com>
2022-02-27 23:08:47 -0300

    remove unused controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show update destroy ]
-
   # GET /posts
   # GET /posts.json
   def index
@@ -10,6 +8,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
   end
 
   # POST /posts
@@ -24,30 +23,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    if @post.update(post_params)
-      render :show, status: :ok, location: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:content, :user_id, :quoted_post_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:content, :user_id, :quoted_post_id)
+  end
 end
--- app/controllers/user_follows_controller.rb
+++ app/controllers/user_follows_controller.rb
@@ -1,17 +1,4 @@
 class UserFollowsController < ApplicationController
-  before_action :set_user_follow, only: %i[ show update destroy ]
-
-  # GET /user_follows
-  # GET /user_follows.json
-  def index
-    @user_follows = UserFollow.all
-  end
-
-  # GET /user_follows/1
-  # GET /user_follows/1.json
-  def show
-  end
-
   # POST /user_follows
   # POST /user_follows.json
   def create
@@ -24,30 +11,16 @@ class UserFollowsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /user_follows/1
-  # PATCH/PUT /user_follows/1.json
-  def update
-    if @user_follow.update(user_follow_params)
-      render :show, status: :ok, location: @user_follow
-    else
-      render json: @user_follow.errors, status: :unprocessable_entity
-    end
-  end
-
   # DELETE /user_follows/1
   # DELETE /user_follows/1.json
   def destroy
-    @user_follow.destroy
+    UserFollow.find(params[:id]).destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user_follow
-      @user_follow = UserFollow.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_follow_params
-      params.require(:user_follow).permit(:follower_id, :followed_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_follow_params
+    params.require(:user_follow).permit(:follower_id, :followed_id)
+  end
 end
